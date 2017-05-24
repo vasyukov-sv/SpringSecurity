871ffae272aedc5ddc1166b35df7c67cd5f89569
vasyukov-sv <vasukovsv@gmail.com>
2017-05-24 10:48:19 +0300

    Reformat PostProcessor

--- src/main/java/ru/javabegin/training/processors/MyBeanPostProcessor.java
+++ src/main/java/ru/javabegin/training/processors/MyBeanPostProcessor.java
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyBeanPostProcessor implements BeanPostProcessor {
 
-	@Override
-	public Object postProcessAfterInitialization(Object object, String name) throws BeansException {
-		System.err.println("postProcessAfterInitialization(): " + object);
-		return object;
-	}
+    @Override
+    public Object postProcessAfterInitialization(Object object, String name) throws BeansException {
+        System.err.println("postProcessAfterInitialization(): " + object);
+        return object;
+    }
 
-	@Override
-	public Object postProcessBeforeInitialization(Object object, String name) throws BeansException {
-		return object;
-	}
+    @Override
+    public Object postProcessBeforeInitialization(Object object, String name) throws BeansException {
+        return object;
+    }
 
 }
